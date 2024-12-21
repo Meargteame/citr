1afc9ed69363ef9e65fc8647481f9c4491e122d9
Meargteame <mearegteame99995555@gmail.com>
2024-12-21 22:38:51 +0300

    Use effect Hook

--- padre-ginos/src/Order.jsx
+++ padre-ginos/src/Order.jsx
@@ -1,109 +1,109 @@
-import Pizza  from "./Pizza";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
+import Pizza from "./Pizza";
 
-
-const intl = new Intl.NumberFormat("en-us",{
-    style:"currency",
-    currency:"USD"
-})
+// feel free to change en-US / USD to your locale
+const intl = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 export default function Order() {
-   
-    const [pizzaTypes,setPizzaTypes]= useState([])
-    const [pizzaType,setPizzaType] = useState("pepperoni");
-    const [pizzaSize,setPizzaSize] = useState("M");
-    const [loading,setLoading] =useState(true);
+  const [pizzaType, setPizzaType] = useState("pepperoni");
+  const [pizzaSize, setPizzaSize] = useState("M");
+  const [pizzaTypes, setPizzaTypes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  let price, selectedPizza;
+  if (!loading) {
+    selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
+    price = intl.format(
+      selectedPizza.sizes ? selectedPizza.sizes[pizzaSize] : "",
+    );
+  }
 
-    return (
-        <div className="order">
-            <h2>Create Order</h2>
-            <form>
-                <div>
-                    <div>
-                        <label htmlFor="pizza-type"> Pizza Type </label>
+  useEffect(() => {
+    fetchPizzaTypes();
+  }, []);
 
-                        <select 
-                            
-                            onChange={(e) =>setPizzaType(e.target.value) }
-                            name="pizza-type" value={pizzaType}>
+  async function fetchPizzaTypes() {
+    const pizzasRes = await fetch("/api/pizzas");
+    const pizzasJson = await pizzasRes.json();
+    setPizzaTypes(pizzasJson);
+    setLoading(false);
+  }
 
-                            <option value="pepperoni">The Pepperoni Pizza</option>
-                            <option value="hawaiian">The Hawaiian Pizza</option>
-                            <option value="big_meat">The Big Meat Pizza</option>
-
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="pizza-size">Pizza Size</label>
-                        <div>
-                            <span>
-                                <input
-                                checked={pizzaSize === 'S'}
-                                type="radio"
-                                name="pizza-size"
-                                value="S"
-                                id="pizza-s"
-                                >
-                                
-                                </input>
-                                <label htmlFor="pizza-s">Small</label>
-                            </span>
-
-                            {/* Medium size */}
-                            <span>
-                                <input
-                                checked={pizzaSize === 'M'}
-                                type="radio"
-                                name="pizza-size"
-                                value="M"
-                                id="pizza-m"
-                                >
-                                
-                                </input>
-                                <label htmlFor="pizza-m">Medium</label>
-                            </span>
-
-                            {/* Large size */}
-                            <span>
-                                <input
-                                checked={pizzaSize === 'L'}
-                                type="radio"
-                                name="pizza-size"
-                                value="L"
-                                id="pizza-l"
-                                >
-                                
-                                </input>
-                                <label htmlFor="pizza-s">Large</label>
-                            </span>
-
-                        </div>
-
-
-                        <div className="order-pizza">
-
-                            <Pizza 
-                            name="Pepperoni"
-                            description="another pep pizza"
-                            // image="/public/pizzas/pepperoni.webp"
-                            image="/pizzas/pepperoni.webp"
-
-                            />
-                            <p>$ 13.75 </p>
-                        </div>
-
-
-
-                    </div>
-                    <button type="submit">Add to cart </button>
-
-
-                   
-
-
-                </div>
-            </form>
+  return (
+    <div className="order">
+      <h2>Create Order</h2>
+      <form>
+        <div>
+          <div>
+            <label htmlFor="pizza-type">Pizza Type</label>
+            <select
+              onChange={(e) => setPizzaType(e.target.value)}
+              name="pizza-type"
+              value={pizzaType}
+            >
+              {pizzaTypes.map((pizza) => (
+                <option key={pizza.id} value={pizza.id}>
+                  {pizza.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="pizza-size">Pizza Type</label>
+            <div>
+              <span>
+                <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
+                  checked={pizzaSize === "S"}
+                  type="radio"
+                  name="pizza-size"
+                  value="S"
+                  id="pizza-s"
+                />
+                <label htmlFor="pizza-s">Small</label>
+              </span>
+              <span>
+                <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
+                  checked={pizzaSize === "M"}
+                  type="radio"
+                  name="pizza-size"
+                  value="M"
+                  id="pizza-m"
+                />
+                <label htmlFor="pizza-m">Medium</label>
+              </span>
+              <span>
+                <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
+                  checked={pizzaSize === "L"}
+                  type="radio"
+                  name="pizza-size"
+                  value="L"
+                  id="pizza-l"
+                />
+                <label htmlFor="pizza-l">Large</label>
+              </span>
+            </div>
+          </div>
+          <button type="submit">Add to Cart</button>
         </div>
-    )
+        {loading ? (
+          <h3>LOADING …</h3>
+        ) : (
+          <div className="order-pizza">
+            <Pizza
+              name={selectedPizza.name}
+              description={selectedPizza.description}
+              image={selectedPizza.image}
+            />
+            <p>{price}</p>
+          </div>
+        )}
+      </form>
+    </div>
+  );
 }
